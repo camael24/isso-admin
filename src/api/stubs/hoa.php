f5f527ea6a1732a20727bda92a89243fb7d288e4
Pierre Tomasina <tomasina@plab.io>
2015-07-26 19:49:14 +0200

    Add Hoa stubs and composer requirement

---
+++ src/api/stubs/hoa.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hoa\Console {
+    use Hoa\Core;
+
+    class Exception extends Core\Exception\Exception {}
+}
+
+namespace Hoa\Core {
+    class Exception extends \Hoa\Core\Exception\Exception {}
+}
