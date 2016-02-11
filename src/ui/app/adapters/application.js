d460847322a8e7101dc9a6dbdf3b858209c55de9
camael24 <thehawk@hoa-project.net>
2016-02-11 23:16:41 +0100

    add url on comment information

--- src/ui/app/adapters/application.js
+++ src/ui/app/adapters/application.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
     namespace: 'api/v1',
-    host: 'http://localhost:8081'
+    host: 'http://localhost:8081',
+    shouldReloadAll() { return true; }
 });
--- src/ui/app/models/post.js
+++ src/ui/app/models/post.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     uri: DS.attr('string'),
     title: DS.attr('string'),
-    comments: DS.hasMany('comments')
+    comments: DS.hasMany('comments', {async: true})
 });
